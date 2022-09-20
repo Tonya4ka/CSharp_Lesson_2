7c188c85d299d7c977e36a769d7825f8efbb12bf
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-20 18:36:25 +0300

    Add Ex_13

---
+++ Ex_13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Type a number: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number >= 100)
+{
+    string thirdDigit = number.ToString();
+    Console.Write($"3-digit of this number is {thirdDigit[2]}");
+}
+else
+{
+    Console.Write("No third digit in this number");
+}
