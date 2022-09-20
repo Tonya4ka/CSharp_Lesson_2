5cb91de03d1a7a65b3ec1ba522f8cbba33cc3498
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-20 17:55:03 +0300

    Add Ex_10 + ignore file

---
+++ Ex_10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Type a 3-digit number: ");
+int number = int.Parse(Console.ReadLine());
+if ((number >= 100) && (number < 1000))
+{
+    int secondDigit = ((number/10)%10);
+    Console.Write(secondDigit);
+}
+else
+{
+    Console.Write("The number is not 3-digital");
+}
